eb166ad7d8724c3ac2f9617b69a3dea3d3b17f9f
zimfv <zimfv@yandex.ru>
2024-10-06 03:24:23 +0300

    Clean the branch

--- sphinx-regenerate.sh
+++
@@ -1,9 +0,0 @@
-# reinstall the package
-pip uninstall bguriver
-python setup.py bdist_wheel sdist
-pip install .
-
-# regenerate the docs
-sphinx-build -M clean docs docs/html
-sphinx-apidoc -o docs src/bguriver
-sphinx-build -M html docs docs
